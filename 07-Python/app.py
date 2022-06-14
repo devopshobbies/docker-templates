a058638bb376f3cdf86b8210cea64a6c1048fc25
ahmadalibagheri <ahmadalibagheri@yahoo.com>
2022-06-14 23:03:47 +0100

    update python code

--- 07-Python/app.py
+++ 07-Python/app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, From DevOps Hobbies!'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="8080")
+    
