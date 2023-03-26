b68790db05446ea164909e6d8bfcf1997e9b4425
Moeid Heidari <moeidheidari@comfortel.pro>
2023-03-26 17:18:01 +0200

    Create bootstrap.sh

---
+++ 01-JavaScript-Typescript-Backend/scripts/bootstrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Exit on error. Append "|| true" if you expect an error.
+set -o errexit
+# Exit on error inside any functions or subshells.
+set -o errtrace
+# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
+set -o nounset
+# Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
+set -o pipefail
+# Turn on traces, useful while debugging but commented out by default
+# set -o xtrace
+
+# Turn on bash's job control
+set -m
+
+__APP_PATH="${MS_HOME:-/app}"
+
+node dist/main.js
