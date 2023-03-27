3bbd9babe353bdba12de6a1bcfc97b64477f2bb7
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-27 15:19:45 +0330

    add[GO]: echo framework for golang example

--- 04-GO/cmd/main.go
+++ 04-GO/cmd/main.go
@@ -1,20 +1,24 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
-func indexHandler(w http.ResponseWriter, _ *http.Request) {
-	_, _ = fmt.Fprintf(w, "We are DevOps Hobbies.")
+func indexHandler(c echo.Context) error {
+	return c.String(http.StatusOK, "We are DevOps Hobbies :)")
 }
 
 func main() {
+	app := echo.New()
+
+	app.GET("/", indexHandler)
+
 	log.Println("We are ready. Then please go to the localhost...")
 
-	http.HandleFunc("/", indexHandler)
-	if err := http.ListenAndServe(":80", nil); err != nil {
+	if err := app.Start(":80"); err != nil {
 		log.Fatal(err)
 	}
 }
