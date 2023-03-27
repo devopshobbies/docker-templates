7a256cedbe8ac5e432f33056ab852283e1d82371
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-27 15:08:15 +0330

    update/add[GO]: main structure / go.mod file

--- 04-GO/cmd/main.go
+++ 04-GO/cmd/main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "We are DevOps Hobbies.")
-
+func indexHandler(w http.ResponseWriter, _ *http.Request) {
+	_, _ = fmt.Fprintf(w, "We are DevOps Hobbies.")
 }
 
 func main() {
-	fmt.Println("We are ready. Then please go to the localhost...")
+	log.Println("We are ready. Then please go to the localhost...")
+
 	http.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":80", nil)
+	if err := http.ListenAndServe(":80", nil); err != nil {
+		log.Fatal(err)
+	}
 }
