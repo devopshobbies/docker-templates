7689f88d94712b1650931a07b927da0075411094
karrabi <a.karrabi@gmail.com>
2023-03-28 08:47:05 +0330

    main.py modified, sample database connection added

--- 07-Python/PythonWithPostgreSQL/project/main.py
+++ 07-Python/PythonWithPostgreSQL/project/main.py
@@ -1,5 +1,21 @@
+import psycopg2
+
+
 def main():
-    print('Hello World!!!')
+
+    try:
+        conn = psycopg2.connect(
+            host="postgres",
+            port="5432",
+            database="postgres",
+            user="postgresuser",
+            password="postgrespassword"
+        )
+        print("Connection successful!")
+    except psycopg2.Error as e:
+        print("Unable to connect to the database:", e)
+    finally:
+        conn.close()
 
 
 if __name__ == '__main__':
