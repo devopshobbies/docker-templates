3903e5660441832042692bb2b135abf1fcaa1d6f
birddevelper <mostafa_shaeri_tj@yahoo.com>
2023-03-30 09:16:21 +0430

    Add java code

---
+++ 05-JAVA/Maven/src/main/java/org/devopshobbies/maven/MavenApplication.java
@@ -0,0 +1,13 @@
+package org.devopshobbies.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenApplication.class, args);
+	}
+
+}
---
+++ 05-JAVA/Maven/src/test/java/org/devopshobbies/maven/MavenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devopshobbies.maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
