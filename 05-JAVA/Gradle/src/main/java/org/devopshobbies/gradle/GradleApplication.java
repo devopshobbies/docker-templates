110df4632ef4b389e64538ca2cfabdc2f621964a
birddevelper <mostafa_shaeri_tj@yahoo.com>
2023-03-31 14:48:11 +0430

    Create Gradle project

---
+++ 05-JAVA/Gradle/src/main/java/org/devopshobbies/gradle/GradleApplication.java
@@ -0,0 +1,13 @@
+package org.devopshobbies.gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleApplication.class, args);
+    }
+
+}
