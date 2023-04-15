b37b45a77e2d574dc8d84603042e7195d2417196
Sms-Rk <smsrk741@gmail.com>
2023-04-15 13:45:55 +0330

    create hello devops program

---
+++ 17-FLutter/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello, From DevOps Hobbies'),
+        ),
+        body: Center(
+          child: Text('Hello, From DevOps Hobbies'),
+        ),
+      ),
+    ),
+  );
+}
